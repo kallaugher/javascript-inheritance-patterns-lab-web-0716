2a899f45a189a2ffd2ca8e85c2d11331efe1a983
kallaugher <alicekallaugher@gmail.com>
2016-09-21 18:02:47 -0400

    redo lab with ES5

--- index.js
+++ index.js
@@ -1,113 +1,202 @@
-class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  toString() {
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+  this.toString = function() {
     return `${this.x}, ${this.y}`
   }
 }
 
-class Side {
-  constructor(length){
-    this.length = length;
-  }
+Point.prototype.toString = function() {
+  return `${this.x}, ${this.y}`
+}
+// class Point {
+//   constructor(x, y) {
+//     this.x = x;
+//     this.y = y;
+//   }
+//   toString() {
+//     return `${this.x}, ${this.y}`
+//   }
+// }
+
+function Side(length) {
+  this.length = length;
+}
+// class Side {
+//   constructor(length){
+//     this.length = length;
+//   }
+// }
+function Shape() {
+  this.position = null;
 }
 
-// ES5 version
-// function Shape() {
-//   this.position = null;
+Shape.prototype.addToPlane = function(x, y) {
+  this.position = new Point(x, y)
+}
+
+Shape.prototype.move = function(x, y) {
+  this.position = new Point(x, y)
+}
+//
+// class Shape {
+//   constructor() {
+//     this.position = null;
+//   }
+//
+//   addToPlane(x, y){
+//     this.position = new Point(x, y)
+//   }
+//
+//   move(x, y){
+//     this.position = new Point(x, y)
+//   }
+// }
+
+function Circle(radius) {
+  Shape.call(this);
+  this.radius = radius;
+}
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.diameter = function(){
+  return this.radius * 2
+}
+
+Circle.prototype.area = function(){
+  return Math.PI * this.radius^2
+}
+
+Circle.prototype.circumference = function(){
+  return 2 * Math.PI * this.radius
+}
+// class Circle extends Shape {
+//   constructor(radius) {
+//     super();
+//     this.radius = radius
+//   }
+//   diameter() {
+//     return this.radius * 2
+//   }
+//   area() {
+//     return Math.PI * this.radius^2
+//   }
+//   circumference() {
+//     return 2 * Math.PI * this.radius
+//   }
+// }
+
+function Polygon(sides) {
+  Shape.call(this);
+  this.sides = sides;
+}
+
+Polygon.prototype = Object.create(Shape.prototype);
+Polygon.prototype.constructor = Polygon;
+
+Polygon.prototype.perimeter = function() {
+  var perimeter = 0
+  this.sides.forEach(side => {
+    perimeter += side.length
+  })
+  return perimeter;
+}
+
+Polygon.prototype.numberOfSides = function() {
+  return this.sides.length;
+}
+// class Polygon extends Shape {
+//   constructor(sides) {
+//     super();
+//     this.sides = sides;
+//   }
+//
+//   perimeter() {
+//     var perimeter = 0
+//     this.sides.forEach(side => {
+//       perimeter += side.length
+//     })
+//     return perimeter;
+//   }
+//
+//   numberOfSides() {
+//     return this.sides.length;
+//   }
+// }
+
+function Quadrilateral(side1, side2, side3, side4) {
+  var sides = [new Side(side1), new Side(side2), new Side(side3), new Side(side4)]
+  Polygon.call(this, sides);
+}
+
+Quadrilateral.prototype = Object.create(Polygon.prototype);
+Quadrilateral.prototype.constructor = Quadrilateral;
+//
+// class Quadrilateral extends Polygon {
+//   constructor(side1, side2, side3, side4) {
+//     super();
+//     this.sides = [new Side(side1), new Side(side2), new Side(side3), new Side(side4)]
+//   }
 // }
 //
-// Shape.prototype.addToPlane = function(x, y) {
-//   this.position = new Point(x, y)
+
+function Triangle(side1, side2, side3) {
+  Polygon.call(this, [new Side(side1), new Side(side2), new Side(side3)]);
+}
+
+Triangle.prototype = Object.create(Polygon.prototype);
+Triangle.prototype.constructor = Triangle;
+
+// class Triangle extends Polygon {
+//   constructor(side1, side2, side3) {
+//     super();
+//     this.sides = [new Side(side1), new Side(side2), new Side(side3)]
+//   }
 // }
 //
-// Shape.prototype.move = function(x, y) {
-//   this.position = new Point(x, y)
+
+function Rectangle(width, height) {
+  Quadrilateral.call(this, width, width, height, height)
+  this.width = width;
+  this.height = height;
+}
+
+Rectangle.prototype = Object.create(Quadrilateral.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+Rectangle.prototype.area = function() {
+  return this.width * this.height;
+}
+
+// class Rectangle extends Quadrilateral {
+//   constructor(width, height) {
+//     super();
+//     this.width = width;
+//     this.height = height;
+//     this.sides = [new Side(width), new Side(width), new Side(height), new Side(height)]
+//   }
+//   area(){
+//     return this.width * this.height;
+//   }
 // }
+//
 
-class Shape {
-  constructor() {
-    this.position = null;
-  }
-
-  addToPlane(x, y){
-    this.position = new Point(x, y)
-  }
-
-  move(x, y){
-    this.position = new Point(x, y)
-  }
+function Square(length) {
+  Rectangle.call(this, length, length);
 }
 
-class Circle extends Shape {
-  constructor(radius) {
-    super();
-    this.radius = radius
-  }
-  diameter() {
-    return this.radius * 2
-  }
-  area() {
-    return Math.PI * this.radius^2
-  }
-  circumference() {
-    return 2 * Math.PI * this.radius
-  }
+Square.prototype = Object.create(Rectangle.prototype);
+Square.prototype.constructor = Square;
+
+Square.prototype.listProperties = function() {
+  return this.hasOwnProperty();
 }
-
-class Polygon extends Shape {
-  constructor(sides) {
-    super();
-    this.sides = sides;
-  }
-
-  perimeter() {
-    var perimeter = 0
-    this.sides.forEach(side => {
-      perimeter += side.length
-    })
-    return perimeter;
-  }
-
-  numberOfSides() {
-    return this.sides.length;
-  }
-}
-
-class Quadrilateral extends Polygon {
-  constructor(side1, side2, side3, side4) {
-    super();
-    this.sides = [new Side(side1), new Side(side2), new Side(side3), new Side(side4)]
-  }
-}
-
-class Triangle extends Polygon {
-  constructor(side1, side2, side3) {
-    super();
-    this.sides = [new Side(side1), new Side(side2), new Side(side3)]
-  }
-}
-
-class Rectangle extends Quadrilateral {
-  constructor(width, height) {
-    super();
-    this.width = width;
-    this.height = height;
-    this.sides = [new Side(width), new Side(width), new Side(height), new Side(height)]
-  }
-  area(){
-    return this.width * this.height;
-  }
-}
-
-class Square extends Rectangle {
-  constructor(length){
-    super(length, length);
-  }
-  listProperties(){
-    return this.hasOwnProperty();
-  }
-}
+// class Square extends Rectangle {
+//   constructor(length){
+//     super(length, length);
+//   }
+//   listProperties(){
+//     return this.hasOwnProperty();
+//   }
+// }
